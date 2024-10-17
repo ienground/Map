059a12afe85edd8db29881eb99b3d1b8e1cbbeb4
IENGROUND <ericano.rhee@gmail.com>
2024-10-17 14:25:10 +0900

    Android에 지도 추가

--- composeApp/src/androidMain/kotlin/zone/ien/map/utils/maps/MapScreen.android.kt
+++ composeApp/src/androidMain/kotlin/zone/ien/map/utils/maps/MapScreen.android.kt
@@ -3,8 +3,13 @@ package zone.ien.map.utils.maps
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.naver.maps.map.NaverMap
+import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import com.naver.maps.map.compose.NaverMap
 
+@OptIn(ExperimentalNaverMapApi::class)
 @Composable
 actual fun MapScreen(modifier: Modifier) {
-//    Naver
+    NaverMap(
+        modifier = modifier
+    )
 }
--- composeApp/src/commonMain/kotlin/zone/ien/map/ui/screens/home/transport/TransportScreen.kt
+++ composeApp/src/commonMain/kotlin/zone/ien/map/ui/screens/home/transport/TransportScreen.kt
@@ -1,15 +1,19 @@
 package zone.ien.map.ui.screens.home.transport
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import io.github.alexzhirkevich.cupertino.CupertinoSearchTextField
+import io.github.alexzhirkevich.cupertino.ExperimentalCupertinoApi
 import zone.ien.map.ui.AppViewModelProvider
 import zone.ien.map.ui.navigation.NavigationDestination
 import zone.ien.map.ui.screens.home.HomeScreenBody
@@ -40,11 +44,27 @@ fun TransportScreen(
     }
 }
 
+@OptIn(ExperimentalCupertinoApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun TransportScreenBody(
     modifier: Modifier = Modifier
 ) {
-    MapScreen(
-        modifier = modifier.fillMaxSize()
-    )
+    Box(
+        modifier = modifier
+    ) {
+        SearchBar(
+            query = "",
+            onQueryChange = {},
+            onSearch = {},
+            active = false,
+            onActiveChange = {},
+            placeholder = {}
+        ) {
+            Text("hi")
+        }
+        MapScreen(
+            modifier = modifier.fillMaxSize()
+        )
+    }
+
 }
